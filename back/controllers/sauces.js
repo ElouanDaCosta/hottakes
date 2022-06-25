10649eec875c9029540e89090c916f1a99c9eb02
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-06-25 09:13:04 +0200

    [ADD] fix the createSauce and getAllSauce, add the getOne Sauce, modifySauce and deleteSauce, only the like is not done

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -1,21 +1,43 @@
-const { response } = require('../app');
-const { create } = require('../models/sauces');
 const sauces = require('../models/sauces');
 
 exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauces);
+    console.log(req.body.sauce)
+    const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
     const sauce = new sauces({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
-        .then(() => res.status(201).json({ message: 'Sauce créée !'}))
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
         .catch(error => res.status(400).json({ error }));
 };
 
 exports.getAllSauce = (req, res, next) => {
     sauces.find()
-        .then(sauce => res.status(200).json({ sauce }))
+        .then((sauce) => res.status(200).json(sauce))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.getOneSauce = (req, res, next) => {
+    sauces.findOne({ _id: req.params.id })
+        .then((sauce) => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
+}
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? 
+    {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    sauces.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.deleteSauce = (req, res, next) => {
+    sauces.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
         .catch(error => res.status(400).json({ error }));
 }
